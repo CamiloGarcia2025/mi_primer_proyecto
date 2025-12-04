7435b67c0ad999889230b7b1670e0c28b9d579e7
CamiloGarcia2025 <jcgarciat@unincca.edu.co>
2025-12-03 20:10:38 -0500

    Primer commit: script hola.py funcionando

---
+++ hola.py
@@ -0,0 +1,6 @@
+# hola.py
+# Autor: Camilo Garcia
+# Descripción: Script básico que saluda al usuario
+
+nombre = input("Camilo Garcia ")
+print(f"¡Hola, {nombre}! Bienvenido a tu primer proyecto con Git y Python.")
